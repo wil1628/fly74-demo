ee470c360b6a10c6deca7c3b549269ef886b9b72
wil1628 <fofojimenez16@gmail.com>
2026-02-07 08:49:37 -0600

    Initial commit

---
+++ src/main/java/com/fly74/fly74_demo/Fly74DemoApplication.java
@@ -0,0 +1,13 @@
+package com.fly74.fly74_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fly74DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fly74DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fly74/fly74_demo/Fly74DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fly74.fly74_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fly74DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
